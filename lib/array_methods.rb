bb588f2608c51945c49743d8e9d03c51895f4673
Kyle Luke <th.oldschool@gmail.com>
2019-07-20 15:33:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,11 @@ def find_element_index(array, value_to_find)
   
   array.length.times { |index|
     if array[index] == value_to_find
-      return index
+      value = index
+      value
     else
-      return nil
+      value = nil 
+      value
     end
   }
   
